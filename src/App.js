109719549b008287d2c5d699efc4d231d58d5540
Ashrf Ali <ashrftvm1@gmail.com>
2019-10-07 13:53:54 +0400

    useeffect nd use state example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -7,12 +7,19 @@ function App() {
 
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("effect has been run");
+  }, [counter]);
+
   return (
     <div className="App">
       <form className="search-form">
         <input type="text" className="search-bar" />
         <button className="search-button">Search</button>
       </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
